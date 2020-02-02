5ab30b7a30d5050a7c96c972394feaf94e3b6cb4
rubpluszz <50639817+rubpluszz@users.noreply.github.com>
2020-02-02 07:32:19 +0100

    Delete bot_message.py

--- bot_message.py
+++
@@ -1,5 +0,0 @@
-class TextMessageClass():
-    start_message = {'en':"Hello I rhyme bot, you input the any word I output rhyme. Let's enter the word.To select a language, enter '/language'",
-                    'uk':"Привіт я поет-бот, введи слово,  а я знайду до нього рими.Давай введи слово. Для зміни мови введи '/language' ",
-                    'ru':"Здрасвуй я бот рифмоплет, вы вводете слово я выдаю рифмы. Поехали введи слово. Для выбора языка введи '/language'",
-                    'pl':"Cześć, ja jestem rymobotem, wpisz słowo a ja znajde do njego rymy.Dawaj wpiszmy słowo. Aby wybrać język, wpisz „/language”",}

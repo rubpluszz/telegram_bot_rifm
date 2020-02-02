0ca3ab64831b6236d82a2bb7fba7721208419dc4
rubpluszz <50639817+rubpluszz@users.noreply.github.com>
2020-02-02 07:32:48 +0100

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,7 +0,0 @@
-import telebot
-class KeyboardsForBot():  
-    
-    keyboard0 = telebot.types.ReplyKeyboardMarkup(True, True)
-    keyboard0.row('/English','/Українська','/Русский','/Polski')
-    
-    

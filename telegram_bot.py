812c1aaf0c704c88727aa5d846fb23fe01f7c33f
rubpluszz <50639817+rubpluszz@users.noreply.github.com>
2020-10-20 04:17:57 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,9 @@ import telebot
 from parsers import ParsersForRyhmes
 from config import token , admin_id
 
+
 bot = telebot.TeleBot(token)
+
 
 def update_language(telegram_id, language):
     """
@@ -19,6 +21,7 @@ def update_language(telegram_id, language):
         connection.close()
     except Exception as e:
         bot_send_error_message_to_admin("update_language",str(e), telegram_id)
+ 
 
 def choose_usser_language(telegram_id, language):
     """
@@ -41,6 +44,7 @@ def choose_usser_language(telegram_id, language):
     except Exception as e:
         bot_send_error_message_to_admin("choose_user_language", str(e), telegram_id)
 
+        
 @bot.message_handler(commands=['start'])
 def start_message(message):
     start_message = {'en':"Hello I rhyme bot, you input the any word I output rhyme. Let's enter the word.To select a (language, enter '/language')",
@@ -49,32 +53,38 @@ def start_message(message):
                     'pl':"Cześć, ja jestem rymobotem, wpisz słowo a ja znajde do njego rymy.Dawaj wpiszmy słowo. (Aby wybrać język, wpisz „/language”)",}
     bot.send_message(message.chat.id, start_message[choose_usser_language(message.from_user.id, message.from_user.language_code)])
 
+    
 @bot.message_handler(commands=['language'])#Select language menu
 def select_language_menu(message):
     keyboard0 = telebot.types.ReplyKeyboardMarkup(True, True)
     keyboard0.row('/English','/Українська','/Русский','/Polski')
     bot.send_message(message.chat.id,'select a language', reply_markup=keyboard0)
 
+    
 @bot.message_handler(commands=['English'])
 def select_english_language(message):
     update_language(message.from_user.id, 'en')
     start_message(message)
 
+    
 @bot.message_handler(commands=['Українська'])
 def select_ukranian_language(message):
     update_language(message.from_user.id, 'uk')
     start_message(message)
 
+    
 @bot.message_handler(commands = ['Русский'])
 def select_russian_language(message):
     update_language(message.from_user.id, 'ru')
     start_message(message)
 
+    
 @bot.message_handler(commands = ['Polski'])
 def select_polish_language(message):
     update_language(message.from_user.id, 'pl')
     start_message(message)
 
+    
 @bot.message_handler(content_types=['text'])
 def analiz_message(message):
     try:
@@ -95,6 +105,7 @@ def analiz_message(message):
     except Exception as e:
         bot_send_error_message_to_admin("analiz_message", str(e), message.from_user.id)
 
+        
 def bot_send_error_message_to_admin(function_name, e , telegram_id):
     bot.send_message(admin_id, f"Erro in the {function_name} - {e}")
     error_message = {'en':"Sorry we made a small mistake. The administrator is already working on this. Try repeating your actions, or try others.",
@@ -103,5 +114,6 @@ def bot_send_error_message_to_admin(function_name, e , telegram_id):
                     'pl':"Przepraszam, mamy mały błąd. Administrator już nad tym pracuje. Spróbuj powtórzyć swoje działania lub wykonać inne.",}
     bot.send_message(telegram_id, error_message(telegram_id))
 
+    
 if __name__ in "__main__":
     bot.polling(none_stop = True)
